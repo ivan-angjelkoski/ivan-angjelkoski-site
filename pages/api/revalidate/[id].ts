dd2bd6f7547e7b1f17a47fb1df0849df7d018e0d
Ivan Angelkoski <56976418+nightexgv@users.noreply.github.com>
2022-12-14 20:55:19 +0100

    Added: Revalidate API Route

---
+++ pages/api/revalidate/[id].ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+	name: string;
+};
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const id = req.query.id;
+	if (!id) {
+		res.status(200).json({ message: 'Error' });
+	} else if (typeof id == 'string') {
+		await res.revalidate(`/blog/${id}`);
+		res.json({ revalidated: true });
+	} else {
+		await res.revalidate(`/blog/${id[0]}`);
+		res.status(200).json({ revalidated: req.query });
+	}
+}
